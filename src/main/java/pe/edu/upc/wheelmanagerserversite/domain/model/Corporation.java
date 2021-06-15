06378cc0b193ba459a7f54c91be7f0671a52b51d
Jose <holzent@gmail.com>
2021-06-15 16:54:54 -0500

    Model Features

---
+++ src/main/java/pe/edu/upc/wheelmanagerserversite/domain/model/Corporation.java
@@ -0,0 +1,38 @@
+package pe.edu.upc.wheelmanagerserversite.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "corporations")
+@Getter
+@Setter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Corporation extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String ruc;
+
+    @NotNull
+    @Size(max = 25)
+    private String name;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(unique = true)
+    private String address;
+
+    @NotNull
+    @Size(max = 20)
+    @Column(unique = true)
+    private String phone;
+}
---
+++ src/main/java/pe/edu/upc/wheelmanagerserversite/domain/model/Product.java
@@ -0,0 +1,56 @@
+package pe.edu.upc.wheelmanagerserversite.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Product extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private int rating;
+
+    @Column(unique = true)
+    private int units_int_stock;
+
+    @NotNull
+    @Size(max = 25)
+    @Column(unique = true)
+    private String name;
+
+    @Lob
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @NotNull
+    @Size(max = 20)
+    @Column(unique = true)
+    private  Double price;
+
+    @Lob
+    @Column(name = "picture", nullable = false)
+    private String picture;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "corporation_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Corporation corporation;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "category_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ProductCategory productCategory;
+}
